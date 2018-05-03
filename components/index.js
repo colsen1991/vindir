719c15f6038dd623c79e75147b962866c5141eb0
Christer Olsen <colsen1991@gmail.com>
2018-05-03 13:16:35 +0200

    Fixed vue-disqus after dependency bump.

--- components/index.js
+++ components/index.js
@@ -1,6 +1,6 @@
 import SiteFooter from './site-footer.vue'
 import SiteHeader from './site-header.vue'
 import PageHeader from './page-header.vue'
-import VueDisqus from 'vue-disqus'
+import VueDisqus from 'vue-disqus/dist/vue-disqus.vue'
 
 export default [ SiteFooter, SiteHeader, PageHeader, VueDisqus ]
